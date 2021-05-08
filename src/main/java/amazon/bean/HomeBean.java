82197a520fe613c5b0ad9e0e15167cde33a71ee7
Divyasree <divya.mende39@gmail.com>
2021-05-08 12:49:37 +0530

    amazon updated by developer warfile

---
+++ src/main/java/amazon/bean/HomeBean.java
@@ -0,0 +1,25 @@
+package amazon.bean;
+
+import com.jsmartframework.web.util.WebText;
+import com.jsmartframework.web.manager.WebContext;
+import com.jsmartframework.web.annotation.WebBean;
+
+import amazon.service.SpringService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebBean
+public class HomeBean {
+
+    @Autowired
+    private SpringService springService;
+
+    public void updateClockTime(String clockTime) {
+		if (!StringUtils.isBlank(clockTime)) {
+			WebContext.addSuccess("feedback", WebText.getString("texts", "async.archetype.clock.updated", clockTime));
+		} else {
+			WebContext.addWarning("feedback", WebText.getString("texts", "async.archetype.clock.error"));
+		}
+    }
+
+}
---
+++ src/main/java/amazon/service/SpringService.java
@@ -0,0 +1,11 @@
+package amazon.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringService {
+
+    public void doSomething() {
+
+    }
+}
